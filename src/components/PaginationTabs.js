838318b014db749c63a3ca51975624505c8781cb
Олександр Тупчієнко <tupchienkoos@gmail.com>
2020-04-04 10:07:09 +0400

    totalPage change to totalPages movies=>movie

--- src/components/PaginationTabs.js
+++ src/components/PaginationTabs.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {Button} from '../../node_modules/semantic-ui-react'
 
 const PaginationTabs = (props) => {
-    const {currentPage,totalPage,onChangeCurrentPage}=props
+    const {currentPage,totalPages,onChangeCurrentPage}=props
     return(
     <div>
         <Button onClick={() => {onChangeCurrentPage (currentPage - 1)  }} type="button" icon={{ name: 'left arrow' }} size='mini' disabled={currentPage === 1 ? true : false} />
         <Button type="button" size='mini' content={currentPage } disabled={true} />...
-        <Button onClick={() => {onChangeCurrentPage( totalPage) }} content={totalPage} size='mini' disabled={currentPage === totalPage?true:false}/>
-        <Button onClick={() => {onChangeCurrentPage (currentPage + 1) }} type="button" size='mini' disabled={currentPage === totalPage ? true : false} icon={{ name: 'right arrow' }} />
+        <Button onClick={() => {onChangeCurrentPage( totalPages) }} content={totalPages} size='mini' disabled={currentPage === totalPages?true:false}/>
+        <Button onClick={() => {onChangeCurrentPage (currentPage + 1) }} type="button" size='mini' disabled={currentPage === totalPages ? true : false} icon={{ name: 'right arrow' }} />
     </div>)
 }
 export default PaginationTabs;
