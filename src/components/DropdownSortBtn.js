c3c205ac62d42d165364405868d0b3b1151ec030
Олександр Тупчієнко <tupchienkoos@gmail.com>
2020-04-04 09:50:32 +0400

    Change dropdownList array

--- src/components/DropdownSortBtn.js
+++ src/components/DropdownSortBtn.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Dropdown } from '../../node_modules/semantic-ui-react'
 
-const dropdownList = [{ key: "Popularity desc", text: "Popularity desc", value: "&sort_by=popularity.desc" },
-                    { key: "Popularity asc", text: "Popularity asc", value: "&sort_by=popularity.asc" },
-                    { key: "Release desc", text: "Release desc", value: "&sort_by=release_date.desc" },
-                    { key: "Revenue desc", text: "Revenue desc", value: "&sort_by=revenue.desc" }]
+const dropdownList = [{ key: "Popularity desc", text: "Popularity desc", value: "popularity.desc" },
+{ key: "Popularity asc", text: "Popularity asc", value: "popularity.asc" },
+{ key: "Release desc", text: "Release desc", value: "release_date.desc" },
+{ key: "Revenue desc", text: "Revenue desc", value: "revenue.desc" }]
 
 
 
@@ -12,14 +12,14 @@ const DropdownSortBtn = (props) => {
 
     return (
         <Dropdown
-            button           
+            button
             className='icon'
             placeholder="Select sort"
             fluid
             labeled
             disabled={!props.isFetched}
             icon='filter'
-            options={dropdownList}            
+            options={dropdownList}
             onChange={(e, data) => props.onChangeSortHandler(data.value)} />)
 }
 export default DropdownSortBtn;
